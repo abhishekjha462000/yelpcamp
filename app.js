e1aa419cd73c204641e17671b421bbdf926b5659
Abhishek jha (دانيال) <sameerjha462000@gmail.com>
2024-12-01 21:29:46 +0530

    .

--- app.js
+++ app.js
@@ -4,17 +4,13 @@ const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
-});
-
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", () => {
-    console.log("Database connected");
-});
+mongoose.connect('mongodb://localhost:27017/yelp-camp')
+    .then(() => {
+        console.log('connected to db');
+    })
+    .catch(() => {
+        console.log('Oh no!!!!!!! we could not connect to the database');
+    });
 
 const app = express();
 
