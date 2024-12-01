7e406edf2256f3df9d6d5c5d0e0af084afac0676
Abhishek jha (دانيال) <sameerjha462000@gmail.com>
2024-12-01 18:23:53 +0530

    #yelcamp_fixed

---
+++ src/seeds/index.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const cities = require('./cities.js');
+const { descriptors, places } = require('./seedHelpers.js');
+const Campground = require('../models/campground.js');
+
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
+    .then(() => {
+        console.log('database connected successfully');
+    })
+    .catch(() => {
+        console.log('Oh no error!!! could not connect to the database');
+    });
+
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    for (let i = 0; i < 50; i++) {
+        const random1000 = Math.floor(Math.random() * 1000);
+        const camp = new Campground({
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`
+        })
+        await camp.save();
+    }
+}
+
+seedDB().then(() => {
+    mongoose.connection.close();
+})
+
